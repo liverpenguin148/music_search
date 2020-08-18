081495e122eb9118d9ee4bf918b3397feb25cc9f
takahiro.usui <liverpenguin1408@gmail.com>
2020-08-19 00:18:33 +0900

    初期設定

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-container>
+      <v-row class="text-center">
+        <v-col class="mb-4">
+          <h1 class="display-2 font-weight-bold mb-3">MUSIC SEARTCH</h1>
+          <p>あなたの音楽を見つけよう</p>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
 }
 </script>
